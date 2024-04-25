b99bbbf7d5402683d0724034a3a38c82b703505f
Joerg Hofrichter <joerg.hofrichter@ni.com>
2024-04-25 08:10:20 +0200

    ChromiumEcPowerDriver: implement different cycle methods
    Signed-off-by: Joerg Hofrichter <joerg.hofrichter@ni.com>

--- labgrid/driver/chromiumecdriver.py
+++ labgrid/driver/chromiumecdriver.py
@@ -19,6 +19,8 @@ class ChromiumEcPowerDriver(Driver, PowerResetMixin, PowerProtocol):
     # priorities = {"SerialDriver2": 10, "SerialDriver": -10, }
     priorities = {"ConsoleProtocol": -10, }
     # on_timeout = attr.ib(default=1, validator=attr.validators.instance_of(int))
+    delay = attr.ib(default=5, validator=attr.validators.instance_of(int))
+    cycle_mode = attr.ib(default="", validator=attr.validators.instance_of(str))
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
@@ -40,10 +42,27 @@ class ChromiumEcPowerDriver(Driver, PowerResetMixin, PowerProtocol):
         self.console.sendline("powerinfo")
         self.console.expect("power state 0 = G3", self.default_timeout)
 
-    def cycle(self):
+    def _cycle_reboot(self):
         self.console.sendline("reboot")
         self.console.expect("UART initialized after reboot", self.default_timeout)
         self.console.expect("Reset cause: reset-pin soft", self.default_timeout)
+        if not self.reboot_autostart:
+            self.console.sendline("powerbtn")
+            time.sleep(self.on_timeout)
         self.console.expect("power state 4 = G3->S5", self.default_timeout)
         self.console.expect("power state 3 = S0", timeout=self.on_timeout)
 
+    def _cycle_apreset(self):
+        self.console.sendline("apreset")
+        self.console.expect("chipset_reset", timeout=self.on_timeout)
+        self.console.expect("power state 3 = S0", timeout=self.on_timeout)
+
+    def cycle(self):
+        if self.cycle_mode == "reboot":
+            self._cycle_reboot()
+        elif self.cycle_mode == "apreset":
+            self._cycle_apreset()
+        else:
+            self.off()
+            time.sleep(self.delay)
+            self.on()
